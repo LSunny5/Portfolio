675710aaef528726268280a8321b4d8bcfd76e8a
Sunny Lee <leesunny507@gmail.com>
2020-01-08 00:34:56 -0500

    mobile start done

---
+++ portfolio.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+function startPage() {
+    console.log('not working');
+    $('.portfolio').hide();
+  
+
+
+
+
+
+}
+
+//function for when user clicks portfolio or aboutme buttons
+
+
+
+
+
+//initialize the quiz
+function initializeQuiz() {
+   // $('.questionPage').hide();
+   // $('.finalPage').hide();
+    //score = 0;
+    //qNumber = 0;
+}
+
+
+
+
+
+
+
+
+
+
+//Javascript Functions to run
+function begin() {
+    startPage();
+
+
+
+}
+
+$(begin);
