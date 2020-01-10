3b83df922525bfdb55392f6ad0e613e5b4f5188f
Sunny Lee <leesunny507@gmail.com>
2020-01-09 22:07:52 -0500

    finished portfolio first round

--- portfolio.js
+++ portfolio.js
@@ -1,40 +1,51 @@
+/*start page and event listeners*/
 function startPage() {
+    /*portfolio button click*/
     $('.start').on('click', '.portfolioButton', function (event){
-        $('.start').hide();
-        $('.aboutMe').hide();
-        $('.portfolio').show();
-        $('.nav').show();
-
-
-
-
+        portfolioPage();
     })
-
+    /*about me button click*/
     $('.start').on('click', '.aboutButton', function (event){
-        $('.start').hide();
-        $('.aboutMe').show();
-        $('.portfolio').hide();
-        $('.nav').show();
+        aboutPage();
     })
 }
 
-
-
+/*listen for clicks on portfolio and about me links at menu bar*/
+function menuBar() {
+    $('.nav').on('click', '.portfolioLink', function(event) {
+        portfolioPage();
+    })
+    $('.nav').on('click', '.aboutLink', function(event) {
+        aboutPage();
+    })
+}
+/*actions to show portfolio page*/
+function portfolioPage() {
+    $('.start').hide();
+    $('.aboutMe').hide();
+    $('.portfolioSection').show();
+    $('.nav').show();
+    $('.portfolioLink').addClass('activePage');
+    $('.aboutLink').removeClass('activePage');
+}
+/*actions to show about me page*/
+function aboutPage() {
+    $('.start').hide();
+        $('.aboutMe').show();
+        $('.portfolioSection').hide();
+        $('.nav').show();
+        $('.aboutLink').addClass('activePage');
+        $('.portfolioLink').removeClass('activePage');
+}
+/*listens for any clicks on the sunny logo*/
 function logoClick() {
     $('.nav').on('click', '.navImage', function (event){
-        $('.portfolio').hide();
+        $('.portfolioSection').hide();
         $('.aboutMe').hide();
         $('.nav').hide();
         $('.start').show();
-
-
     })
 }
-
-
-
-
-
 
 
 
@@ -42,8 +53,7 @@ function logoClick() {
 function begin() {
     startPage();
     logoClick();
-
-
+    menuBar();
 }
 
 $(begin);
