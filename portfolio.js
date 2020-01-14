a1f191269e26b7f961572e7e7001b801713cfb4a
Sunny Lee <leesunny507@gmail.com>
2020-01-13 23:47:06 -0500

    changed few errors

--- portfolio.js
+++ portfolio.js
@@ -1,21 +1,21 @@
 /*start page and event listeners*/
 function startPage() {
     /*portfolio button click*/
-    $('.start').on('click', '.portfolioButton', function (event){
+    $('.start').on('click', '.portfolioButton', function (event) {
         portfolioPage();
     })
     /*about me button click*/
-    $('.start').on('click', '.aboutButton', function (event){
+    $('.start').on('click', '.aboutButton', function (event) {
         aboutPage();
     })
 }
 
 /*listen for clicks on portfolio and about me links at menu bar*/
 function menuBar() {
-    $('.nav').on('click', '.portfolioLink', function(event) {
+    $('.nav').on('click', '.portfolioLink', function (event) {
         portfolioPage();
     })
-    $('.nav').on('click', '.aboutLink', function(event) {
+    $('.nav').on('click', '.aboutLink', function (event) {
         aboutPage();
     })
 }
@@ -27,19 +27,21 @@ function portfolioPage() {
     $('.nav').show();
     $('.portfolioLink').addClass('activePage');
     $('.aboutLink').removeClass('activePage');
+    $('footer').show();
 }
 /*actions to show about me page*/
 function aboutPage() {
     $('.start').hide();
-        $('.aboutMe').show();
-        $('.portfolioSection').hide();
-        $('.nav').show();
-        $('.aboutLink').addClass('activePage');
-        $('.portfolioLink').removeClass('activePage');
+    $('.aboutMe').show();
+    $('.portfolioSection').hide();
+    $('.nav').show();
+    $('.aboutLink').addClass('activePage');
+    $('.portfolioLink').removeClass('activePage');
+    $('footer').hide();
 }
 /*listens for any clicks on the sunny logo*/
 function logoClick() {
-    $('.nav').on('click', '.navImage', function (event){
+    $('.nav').on('click', '.navImage', function (event) {
         $('.portfolioSection').hide();
         $('.aboutMe').hide();
         $('.nav').hide();
