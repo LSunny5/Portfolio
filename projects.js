d7e74d66cf4348d168df8fffb5b3a88d2b06e1b7
Sunny Lee <leesunny507@gmail.com>
2020-02-03 17:14:57 -0500

    added project info

---
+++ projects.js
@@ -0,0 +1,25 @@
+//store for projects for portfolio
+const PROJECTS = [
+    //Quiz project
+    {
+        title: 'Disney Princess Quiz', 
+        tech: ['html5', 'css3', 'javascript', 'jquery'], 
+        screenshot: 'images/princessQuiz.png', 
+        alt: 'Disney Princess Quiz App Screenshot of Question',
+        description: ' Take this quiz to find out how well you know some of the Disney Princesses, but watch out! If you do badly, Ursula waits!  This quiz is a fun test for anyone young at heart on their knowledge about the princesses.  I built it for my daughters who love Disney Princess trivia.',
+        repo: 'https://github.com/LSunny5/QuizApp', 
+        code: 'https://lsunny5.github.io/QuizApp/'
+    },
+    
+    //API Capstone Project
+    {
+        title: 'Drug Compare App', 
+        tech: ['html5', 'css3', 'javascript', 'jquery'], 
+        screenshot: 'images/DrugCompareResultScreenSS.png', 
+        alt: 'Drug Compare App Result Screen Screenshot',
+        description: 'Try this app to see how much you pay for your pharmacy drug compared to the set standards of the government, manufacturer, and pharmacy.  This quiz is good for anyone who wonders about the pricing of pharamcy drugs.  I built it for my curious family and friends.',
+        repo: 'https://lsunny5.github.io/Drug-Price-Compare/', 
+        code: 'https://github.com/LSunny5/Drug-Price-Compare'
+    }
+
+]
